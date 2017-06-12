3dc2b28170a52820b71d9b48895f719062950b3d
Xantomen <xantomen@gmail.com>
2017-06-12 12:51:32 +0200

    Cleaning up explorer from default API Calls considered unnecessary for the time being

--- backend/common/models/campaign.js
+++ backend/common/models/campaign.js
@@ -2,22 +2,73 @@
 
 module.exports = function(Campaign) {
 		
-	Campaign.disableRemoteMethodByName('create');
+	//Campaign.disableRemoteMethodByName('create');
 	Campaign.disableRemoteMethodByName('replaceOrCreate');
 	Campaign.disableRemoteMethodByName('patchOrCreate');
 	Campaign.disableRemoteMethodByName('exists');
-	Campaign.disableRemoteMethodByName('findById');
-	Campaign.disableRemoteMethodByName('find');
-	Campaign.disableRemoteMethodByName('findOne');
-	Campaign.disableRemoteMethodByName('deleteById');
-	Campaign.disableRemoteMethodByName('count');
+	//Campaign.disableRemoteMethodByName('findById');
+	//Campaign.disableRemoteMethodByName('find');
+	//Campaign.disableRemoteMethodByName('findOne');
+	Campaign.disableRemoteMethodByName('destroyById');
+	//Campaign.disableRemoteMethodByName('count');
 	Campaign.disableRemoteMethodByName('replaceById');
 	Campaign.disableRemoteMethodByName('prototype.patchAttributes');
 	Campaign.disableRemoteMethodByName('createChangeStream');
 	Campaign.disableRemoteMethodByName('updateAll');
+	Campaign.disableRemoteMethodByName('upsertWithWhere');
+						
+	//Campaign.disableRemoteMethodByName('prototype.__get__country_list');
+	Campaign.disableRemoteMethodByName('prototype.__create__country_list');
+	Campaign.disableRemoteMethodByName('prototype.__update__country_list');
+	Campaign.disableRemoteMethodByName('prototype.__destroy__country_list');
+	Campaign.disableRemoteMethodByName('prototype.__delete__country_list');
+	Campaign.disableRemoteMethodByName('prototype.__exists__country_list');
+	//Campaign.disableRemoteMethodByName('prototype.__findById__country_list');
+	Campaign.disableRemoteMethodByName('prototype.__destroyById__country_list');
+	Campaign.disableRemoteMethodByName('prototype.__count__country_list');
+	Campaign.disableRemoteMethodByName('prototype.__updateById__country_list');
+	//Campaign.disableRemoteMethodByName('prototype.__link__country_list');
+	//Campaign.disableRemoteMethodByName('prototype.__unlink__country_list');
 	
-	Campaign.disableRemoteMethodByName('prototype.__create__tags');
-		
+	//Campaign.disableRemoteMethodByName('prototype.__get__constituency_list');
+	Campaign.disableRemoteMethodByName('prototype.__create__constituency_list');
+	Campaign.disableRemoteMethodByName('prototype.__update__constituency_list');
+	Campaign.disableRemoteMethodByName('prototype.__destroy__constituency_list');
+	Campaign.disableRemoteMethodByName('prototype.__delete__constituency_list');
+	Campaign.disableRemoteMethodByName('prototype.__exists__constituency_list');
+	//Campaign.disableRemoteMethodByName('prototype.__findById__constituency_list');
+	Campaign.disableRemoteMethodByName('prototype.__destroyById__constituency_list');
+	Campaign.disableRemoteMethodByName('prototype.__count__constituency_list');
+	Campaign.disableRemoteMethodByName('prototype.__updateById__constituency_list');
+	//Campaign.disableRemoteMethodByName('prototype.__link__constituency_list');
+	//Campaign.disableRemoteMethodByName('prototype.__unlink__constituency_list');
+	
+	//Campaign.disableRemoteMethodByName('prototype.__get__call_list');
+	Campaign.disableRemoteMethodByName('prototype.__create__call_list');
+	Campaign.disableRemoteMethodByName('prototype.__update__call_list');
+	Campaign.disableRemoteMethodByName('prototype.__destroy__call_list');
+	Campaign.disableRemoteMethodByName('prototype.__delete__call_list');
+	Campaign.disableRemoteMethodByName('prototype.__exists__call_list');
+	//Campaign.disableRemoteMethodByName('prototype.__findById__call_list');
+	Campaign.disableRemoteMethodByName('prototype.__destroyById__call_list');
+	Campaign.disableRemoteMethodByName('prototype.__count__call_list');
+	Campaign.disableRemoteMethodByName('prototype.__updateById__call_list');
+	//Campaign.disableRemoteMethodByName('prototype.__link__call_list');
+	//Campaign.disableRemoteMethodByName('prototype.__unlink__call_list');
+	
+	//Campaign.disableRemoteMethodByName('prototype.__get__parliamentarian_list');
+	Campaign.disableRemoteMethodByName('prototype.__create__parliamentarian_list');
+	Campaign.disableRemoteMethodByName('prototype.__update__parliamentarian_list');
+	Campaign.disableRemoteMethodByName('prototype.__destroy__parliamentarian_list');
+	Campaign.disableRemoteMethodByName('prototype.__delete__parliamentarian_list');
+	Campaign.disableRemoteMethodByName('prototype.__exists__parliamentarian_list');
+	//Campaign.disableRemoteMethodByName('prototype.__findById__parliamentarian_list');
+	Campaign.disableRemoteMethodByName('prototype.__destroyById__parliamentarian_list');
+	Campaign.disableRemoteMethodByName('prototype.__count__parliamentarian_list');
+	Campaign.disableRemoteMethodByName('prototype.__updateById__parliamentarian_list');
+	//Campaign.disableRemoteMethodByName('prototype.__link__parliamentarian_list');
+	//Campaign.disableRemoteMethodByName('prototype.__unlink__parliamentarian_list');
+			
 	var app = require('../../server/server');
 	
 	Campaign.linkConstituenciesByNameSegment = function(campaign_id,name_segment,cb) {
